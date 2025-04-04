c68e38f9d61d34225446ce247a573d6726257a28
jaider <jaidergomez20@gmail.com>
2025-04-04 17:53:31 -0500

    Se modifico database para enciar los datos a la nube con SupaBase

--- config/database.php
+++ config/database.php
@@ -1,11 +1,20 @@
 <?php
 
-    //config connection
+    //config connection con supabase
+
+    $host     = "aws-0-us-east-1.pooler.supabase.com";
+    $port     = "6543";
+    $dbname   = "postgres";
+    $user     = "postgres.vtmqzbggujmmotnuyypm";
+    $password = "unicesmag@@";
+    
+    /*
     $host     = "localhost";
     $port     = "5432";
     $dbname   = "schoolar";
     $user     = "postgres";
     $password = "unicesmag";
+    */
 
     //Create connection 
     $conn = pg_connect("
